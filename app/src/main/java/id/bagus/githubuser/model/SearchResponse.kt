f982e4fa4c83b288c80276d3b0984ddb94524ac2
yoihenken <18104005@ittelkom-pwt.ac.id>
2021-04-14 11:56:51 +0700

    add model

---
+++ app/src/main/java/id/bagus/githubuser/model/SearchResponse.kt
@@ -0,0 +1,14 @@
+package id.bagus.githubuser.model
+
+import com.google.gson.annotations.SerializedName
+
+data class SearchResponse(
+	@field:SerializedName("total_count")
+	val totalCount: Int? = null,
+
+	@field:SerializedName("incomplete_results")
+	val incompleteResults: Boolean? = null,
+
+	@field:SerializedName("items")
+	val items: List<UserResponse>? = null
+)
