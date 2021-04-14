5266e9335f4b6983e6e41f427111881d9b0e08f4
yoihenken <18104005@ittelkom-pwt.ac.id>
2021-04-14 11:58:08 +0700

    Update SplashscreenActivity.kt

--- app/src/main/java/id/bagus/githubuser/ui/splashscreen/SplashscreenActivity.kt
+++ app/src/main/java/id/bagus/githubuser/ui/splashscreen/SplashscreenActivity.kt
@@ -4,11 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import id.bagus.githubuser.R
 import id.bagus.githubuser.databinding.ActivitySplashscreenBinding
-import id.bagus.githubuser.model.User
 import id.bagus.githubuser.ui.dashboard.DashboardActivity
-import id.bagus.githubuser.ui.dashboard.DashboardActivity.Companion.EXTRA_USER
 
 class SplashscreenActivity : AppCompatActivity() {
     private lateinit var binding : ActivitySplashscreenBinding
@@ -18,31 +15,9 @@ class SplashscreenActivity : AppCompatActivity() {
         binding = ActivitySplashscreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //get data from string
-        val username = resources.getStringArray(R.array.username)
-        val name = resources.getStringArray(R.array.name)
-        val location = resources.getStringArray(R.array.location)
-        val repository = resources.getStringArray(R.array.repository)
-        val company = resources.getStringArray(R.array.company)
-        val followers = resources.getStringArray(R.array.followers)
-        val following = resources.getStringArray(R.array.following)
-        val avatar = resources.getStringArray(R.array.avatar)
-
-        //create get object
-        val dataUser = arrayListOf<User>()
-        for (i in username.indices){
-            dataUser.add(
-                User(
-                    username[i], name[i], location[i], repository[i], company[i], followers[i], following[i], avatar[i]
-                )
-            )
-        }
-
         Handler(mainLooper).postDelayed({
             finish() //end activity
-            startActivity(Intent(this, DashboardActivity::class.java).apply {
-                putExtra(EXTRA_USER, dataUser)
-            }) //move intent
+            startActivity(Intent(this, DashboardActivity::class.java)) //move intent
         }, 3000)
 
     }
